43ab06c3313ec7a86f5a05c55ff366cf2085ab67
DepressedYoshi <jl2718281828459045@gmail.com>
2024-06-09 20:52:21 -0700

    added food object update object material to look better decrease spwn count for balance Need to implemetn unger and energy level on play in future need to omplemetn ratracing for food in future

--- Food system/FoodSpawner.cs
+++ Food system/FoodSpawner.cs
@@ -1,16 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeSpawner : MonoBehaviour
+public class FoodSpawner : MonoBehaviour
 {
-    public GameObject cubePrefab;
-    public int poolSize = 100;
-    private List<GameObject> cubes;
+    public GameObject foodPrefab;
+    public int poolSize = 200 ;
+    private List<GameObject> Food;
     
     private Terrain terrain;
     private Vector3 terrainSize;
     private Vector3 terrainPosition;
-    private float cubeHeightOffset = 1f; // Half the cube's height to adjust spawn position
 
 
     void Start()
@@ -20,33 +19,33 @@ public class CubeSpawner : MonoBehaviour
         terrainPosition = terrain.transform.position;
         
         // Initialize the cube pool
-        InitializeCubePool();
+        InitializeFoodPool();
         // Spawn cubes at random positions
         SpawnCubes();
     }
 
-    void InitializeCubePool()
+    void InitializeFoodPool()
     {
-        cubes = new List<GameObject>();
+        Food = new List<GameObject>();
         for (int i = 0; i < poolSize; i++)
         {
-            GameObject cube = Instantiate(cubePrefab);
-            cube.SetActive(false);
-            cubes.Add(cube);
+            GameObject food = Instantiate(foodPrefab);
+            food.SetActive(false);
+            Food.Add(food);
         }
     }
 
     void SpawnCubes()
     {
-        foreach (GameObject cube in cubes)
+        foreach (GameObject food in Food)
         {
             float x = Random.Range(terrainPosition.x, terrainPosition.x + terrainSize.x);
             float z = Random.Range(terrainPosition.z, terrainPosition.z + terrainSize.z);
-            float y = terrain.SampleHeight(new Vector3(x, 0, z)) + terrainPosition.y + cubeHeightOffset;
+            float y = terrain.SampleHeight(new Vector3(x, 0, z)) + terrainPosition.y;
             Vector3 spawnPosition = new Vector3(x, y, z);
             
-            cube.transform.position = spawnPosition;
-            cube.SetActive(true);
+            food.transform.position = spawnPosition;
+            food.SetActive(true);
         }
     }
 }
